50c924385cdd3f4232d03f581e7870fc637df28b
zhy <zhy@itheima.cn>
2020-12-06 10:11:58 +0800

    第一次提交

---
+++ assets/js/login.js
@@ -0,0 +1,12 @@
+$(function () {
+    //点击去注册账号
+    $('#link_reg').on('click', function () {
+        $('.login-box').hide();
+        $('.reg-box').show();
+    })
+
+    $('#link_login').on('click', function () {
+        $('.reg-box').hide();
+        $('.login-box').show();
+    })
+})
